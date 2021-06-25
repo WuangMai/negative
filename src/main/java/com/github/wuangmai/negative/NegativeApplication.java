0a3819b4104f4bf2b79cc46e197977d36c702df4
wuang <wuangmai@gmail.com>
2021-06-25 15:25:29 +0200

    initial

---
+++ src/main/java/com/github/wuangmai/negative/NegativeApplication.java
@@ -0,0 +1,13 @@
+package com.github.wuangmai.negative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wuangmai/negative/NegativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wuangmai.negative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
